b43ccc0c2cfaffff3a921d11f5df9d2088cc40a8
agentraghav <raghvendrarathore2@gmail.com>
2021-01-07 14:59:17 +0530

    navigation bar

--- src/nav.js
+++ src/nav.js
@@ -1,27 +1,29 @@
-function navBar() {
-  const contentContainer = document.getElementById('content');
-  const navi = document.createElement('div');
-  navi.setAttribute('id', 'nav');
-  navi.innerHTML = ` <nav class="navbar navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-    </ul>
-  </div>
-</nav>`;
-  contentContainer.appendChild(navi);
+function createLink(id, name) {
+  const link = document.createElement('a');
+  link.setAttribute('id', id);
+  link.setAttribute('href', '#');
+  const span = document.createElement('span');
+  span.textContent = name;
+  link.appendChild(span);
+  return link;
 }
 
-export { navBar };
+function navBar(id) {
+  const nav = document.createElement('nav');
+  nav.setAttribute('id', id);
+  const home = createLink('home', 'Home');
+  const menu = createLink('menu', 'Menu');
+  const contact = createLink('contact', 'Contact');
+  nav.appendChild(home);
+  nav.appendChild(menu);
+  nav.appendChild(contact);
+  return nav;
+}
+
+function loadPage() {
+  const container = document.getElementById('content');
+  const nav = navBar('nav');
+  container.appendChild(nav);
+}
+
+export default loadPage;
