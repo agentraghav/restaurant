55420a2c8f9b34deb1a511e16ea03c262232529d
agentraghav <raghvendrarathore2@gmail.com>
2021-01-07 12:31:53 +0530

    nav bar

--- src/nav.js
+++ src/nav.js
@@ -1,6 +1,16 @@
-const nav = () => {
+const navBar = () => {
   const contentContainer = document.querySelector('#content');
   const menu = document.createElement('div');
   menu.setAttribute('id', 'nav');
-  menu.innerHTML = '';
+  menu.innerHTML = ` <div class="navbar-header">
+<a class="navbar-brand page-scroll" href="#">Manas</a> 
+</div> 
+<div class="collapse navbar-collapse" id="myNav">
+<ul class=" nav navbar-nav pull-right">
+<li><a href="#about" class="page-scroll">About</a></li>
+<li><a href="#portfolio" class="page-scroll">Portfolio</a></li>
+<li><a href="#contact" class="page-scroll">Contact</a></li>
+</ul>
+</div>`;
 };
+export { navBar };
