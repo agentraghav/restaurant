28fe4d172c842462af69f93c6c99d83bc4a87df0
agentraghav <raghvendrarathore2@gmail.com>
2021-01-07 14:37:16 +0530

    test

--- src/nav.js
+++ src/nav.js
@@ -1,17 +1,27 @@
 const navBar = (() => {
-  const contentContainer = document.querySelector('#content');
-  const menu = document.createElement('div');
-  menu.setAttribute('id', 'nav');
-  menu.innerHTML = ` <div class="navbar-header">
-<a class="navbar-brand page-scroll" href="#">Manas</a> 
-</div> 
-<div class="collapse navbar-collapse" id="myNav">
-<ul class=" nav navbar-nav pull-right">
-<li><a href="#about" class="page-scroll">About</a></li>
-<li><a href="#portfolio" class="page-scroll">Portfolio</a></li>
-<li><a href="#contact" class="page-scroll">Contact</a></li>
-</ul>
-</div>`;
-  contentContainer.appendChild(menu);
+  const contentContainer = document.getElementById('content');
+  const navi = document.createElement('div');
+  navi.setAttribute('id', 'nav');
+  navi.innerHTML = ` <nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div>
+</nav>`;
+  contentContainer.appendChild(navi);
 })();
+
 export { navBar };
